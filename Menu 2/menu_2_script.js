3083960df664359f4e9b61b4906d83a0008a46f2
a17towan <a17towan@student.his.se>
2021-03-22 13:13:32 +0100

    Building the second menu
    Building the second menu

--- Menu 2/menu_2_script.js
+++ Menu 2/menu_2_script.js
@@ -12,3 +12,15 @@ $(document).on('click', '.navbar',(function(){
 // Single Page Application
 document.getElementById("omOssSida").style="display:none";
 document.getElementById("kontaktSida").style="display:none";
+
+//Hide nested categories
+document.getElementById("menuChild").style="display:none";
+document.getElementById("menuChildBread").style="display:none";
+document.getElementById("menuChildCake").style="display:none";
+document.getElementById("menuChildDrink").style="display:none";
+document.getElementById("cupcakesChild").style="display:none";
+document.getElementById("creamChild").style="display:none";
+document.getElementById("iceChild").style="display:none";
+document.getElementById("themeChild").style="display:none";
+document.getElementById("coldChild").style="display:none";
+document.getElementById("hotChild").style="display:none";
