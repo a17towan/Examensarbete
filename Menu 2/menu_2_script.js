28ab05791b25d9470751af030784e296ec81d1af
a17towan <a17towan@student.his.se>
2021-03-24 17:56:59 +0100

    Added functions for 'go back' for deeper categories
    Added functions for 'go back' for deeper categories. Also added the style of the 'back' arrow.

--- Menu 2/menu_2_script.js
+++ Menu 2/menu_2_script.js
@@ -1,12 +1,11 @@
 
 // Toggle menu up & down
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
-
 $(document).on('click', '.navbar',(function(){
   $("#menu").slideToggle("fast");
-    console.log("yo");
+}));
+// Close menu upon click on body
+$(document).on('click', '.ark',(function(){
+  $("#menu").slideUp("fast");
 }));
 
 // Single Page Application
@@ -25,17 +24,69 @@ document.getElementById("themeChild").style="display:none";
 document.getElementById("coldChild").style="display:none";
 document.getElementById("hotChild").style="display:none";
 
-// Test the menu 'sortiment'
+// Click on 'sortiment' ----------
 $(document).on('click', '#products',(function(){
   $("#menuParent").toggle('slide');
   $("#menuChild").delay(420).effect('slide', { direction:'right'}, 500);
 
 }));
-
-// Back arrow first level
+// Back arrow first level ----------
 $(document).on('click', '#backArrow',(function(){
-  console.log("hey");
   $("#menuChild").toggle('slide', { direction:'right'});
   $("#menuParent").delay(420).effect('slide', { direction:'left'}, 500);
 }));
+// Click on 'bröd'
+$(document).on('click', '#bread',(function(){
+  $("#menuChild").toggle('slide');
+  $("#menuChildBread").delay(420).effect('slide', { direction:'right'}, 500);
+}))
+// Click on 'tårtor'
+$(document).on('click', '#cakes',(function(){
+  $("#menuChild").toggle('slide');
+  $("#menuChildCake").delay(420).effect('slide', { direction:'right'}, 500);
+}))
+// Click on 'dryck'
+$(document).on('click', '#drinks',(function(){
+  $("#menuChild").toggle('slide');
+  $("#menuChildDrink").delay(420).effect('slide', { direction:'right'}, 500);
+}))
+// Back arrow second level ----------
+$(document).on('click', '#backArrowChild',(function(){
+  $("#menuChildBread").hide('slide', { direction:'right'});
+  $("#menuChildCake").hide('slide', { direction:'right'});
+  $("#menuChildDrink").hide('slide', { direction:'right'});
+  $("#menuChild").delay(420).effect('slide', { direction:'left'}, 500);
+}));
+//Click on 'ljust bröd'
 
+// Click on 'mörkt bröd'
+
+// Click on 'cupcakes'
+$(document).on('click', '#cupcakes',(function(){
+  $("#menuChildCake").toggle('slide');
+  $("#cupcakesChild").delay(420).effect('slide', { direction:'right'}, 500);
+}))
+
+// Click on 'gräddtårtor'
+$(document).on('click', '#creamcakes',(function(){
+  $("#menuChildCake").toggle('slide');
+  $("#creamChild").delay(420).effect('slide', { direction:'right'}, 500);
+}))
+// Click on 'glasstårtor'
+$(document).on('click', '#icecreamcakes',(function(){
+  $("#menuChildCake").toggle('slide');
+  $("#iceChild").delay(420).effect('slide', { direction:'right'}, 500);
+}))
+// Click on 'tårtor med tema'
+$(document).on('click', '#themecakes',(function(){
+  $("#menuChildCake").toggle('slide');
+  $("#themeChild").delay(420).effect('slide', { direction:'right'}, 500);
+}))
+// Back arrow 'tårtor' level ----------
+$(document).on('click', '#backArrowGrandchild',(function(){
+  $("#cupcakesChild").hide('slide', { direction:'right'});
+  $("#creamChild").hide('slide', { direction:'right'});
+  $("#iceChild").hide('slide', { direction:'right'});
+  $("#themeChild").hide('slide', { direction:'right'});
+  $("#menuChildCake").delay(420).effect('slide', { direction:'left'}, 500);
+}));
