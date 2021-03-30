0116c06eeaf165d8d737896309ed6a7c55591211
a17towan <49142028+a17towan@users.noreply.github.com>
2021-03-30 21:07:28 +0200

    Changed from timestamp to varchar
    On second thought, it is better to use varchar so that it is stored as a string because of the way it is formatted on the website

--- Database/examensarbete_databas.sql
+++ Database/examensarbete_databas.sql
@@ -4,30 +4,30 @@ use a17towan_examensarbete;
 
 create table test1( -- Code tabell 
 deltagareID int,
-startTid timestamp,
-slutTid timestamp,,
+startTid varchar (100),
+slutTid varchar (100),
 primary key (deltagareID)
 )engine=innodb;
 
 create table test2( -- Code tabell 
 ID int,
-startTid timestamp,
-slutTid timestamp,
+startTid varchar (100),
+slutTid varchar (100),
 primary key (ID),
 foreign key (ID) references test1(deltagareID)
 )engine=innodb;
 
 create table test3( -- Code tabell 
 ID int,
-startTid timestamp,
-slutTid timestamp,
+startTid varchar (100),
+slutTid varchar (100),
 primary key (ID)
 )engine=innodb;
 
 create table test4( -- Code tabell 
 ID int,
-startTid timestamp,
-slutTid timestamp,
+startTid varchar (100),
+slutTid varchar (100),
 primary key (ID)
 )engine=innodb;
 
