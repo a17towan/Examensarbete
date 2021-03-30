7376ef56cb2c180ba0679c495379bbbe02a04b60
a17towan <49142028+a17towan@users.noreply.github.com>
2021-03-30 17:02:51 +0200

    Updating the database from int to timestamp
    Want to store the start and end timestamps instead of the calculated time difference

--- Database/examensarbete_databas.sql
+++ Database/examensarbete_databas.sql
@@ -4,28 +4,32 @@ use a17towan_examensarbete;
 
 create table test1( -- Code tabell 
 deltagareID int,
-test1 int,
+startTid timestamp,
+slutTid timestamp,,
 primary key (deltagareID)
 )engine=innodb;
 
 create table test2( -- Code tabell 
 ID int,
-test2 int,
+startTid timestamp,
+slutTid timestamp,
 primary key (ID),
 foreign key (ID) references test1(deltagareID)
 )engine=innodb;
 
 create table test3( -- Code tabell 
 ID int,
-test3 int,
+startTid timestamp,
+slutTid timestamp,
 primary key (ID)
 )engine=innodb;
 
 create table test4( -- Code tabell 
 ID int,
-test4 int,
+startTid timestamp,
+slutTid timestamp,
 primary key (ID)
 )engine=innodb;
 
-INSERT INTO test1 (deltagareID, test1) values ('1', '10');
+INSERT INTO test1 (deltagareID, test1, slutTid) values ('1', '2021-01-01 00:00:01', '2021-01-01 00:00:10');
 SELECT * FROM test1;
