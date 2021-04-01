c5ad80639225dd517ce208a5848a5aea24f500ec
a17towan <a17towan@student.his.se>
2021-04-01 15:52:28 +0200

    Added a new file for HTML database insertion
    To insert data from the website to the database, Ajax is used with PHP. A HTML site contains the form which is sent with Ajax to the PHP site, which sends it to the database.

--- Timer & Database Test/timer_and_database_JS.js
+++ Timer & Database Test/timer_and_database_JS.js
@@ -1,43 +1,26 @@
-
-var firstTime; //Global
-var secondTime;
-var array = [];
-var text;
-
-function initialTime(){
-	var today = new Date();
-	var test = today.valueOf()/1000;
-
-	var year = today.getFullYear();
-	var month = today.getDate();
-	var day = today.getDay();
-	var hour = today.getHours();
-	var minute = today.getMinutes();
-	var second = today.getSeconds();
-	var millisecond = today.getMilliseconds();
-
-	var time = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + ':' + millisecond;
-	document.getElementById("timerWindow").innerHTML = 	time;
-	//document.getElementById("startTid").value = time;
-	firstTime=today;
-	document.getElementById("startTid").value = time;
-}
-
-function showTimeDiff(){
-	var timed = new Date();
-	var test = timed.valueOf()/1000;
-
-	var year = timed.getFullYear();
-	var month = timed.getDate();
-	var day = timed.getDay();
-	var hour = timed.getHours();
-	var minute = timed.getMinutes();
-	var second = timed.getSeconds();
-	var millisecond = timed.getMilliseconds();
-
-	var time = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + ':' + millisecond;
-	document.getElementById("secondTimerWindow").innerHTML = time; 
-	//document.getElementById("slutTid").value = time;
-	secondTime=timed;
-	document.getElementById("slutTid").value = time;
-}
+function insert(deltagareID, startTid, slutTid) {
+	return $.ajax({
+		type: "POST",
+		url: "./timer_and_database_PHP.php",
+		data: { 
+			deltagareID,
+			startTid,
+			slutTid
+		}
+	});
+  }
+  
+  const form = document.querySelector("form");
+  form.addEventListener("submit", e => {
+	e.preventDefault();
+	console.log("SUBMIT NO REFRESH");
+	const deltagareID = document.querySelector(`[name="deltagareID"]`).value;
+	const startTid = document.querySelector(`[name="startTid"]`).value;
+	const slutTid = document.querySelector(`[name="slutTid"]`).value;
+	insert(
+	  deltagareID, startTid, slutTid
+	).done((response) => {
+	  console.log(response);
+	});
+  });
+  
--- Timer & Database Test/timer_and_database_PHP.php
+++ Timer & Database Test/timer_and_database_PHP.php
@@ -1,77 +1,35 @@
-<!DOCTYPE html>
-	<html>
-	<head>
-		<style>
-			table{
-				border:1px solid black;
-			}
-			tr{
-				border:1px solid black;
-			}
-			td{
-				border:1px solid black;
-			}
-			th{
-				border:1px solid black;
-			}
-			#calculate{
-				display:none;
-			}
+<?php
+	$host = "wwwlab.iit.his.se";
+	$dbname = "a17towan_examensarbete";
+	$username = "sqllab";
+	$password = "Tomten2009";
+	$dsn = "mysql:host=" . $host . ";dbname=" . $dbname . ";charset=utf8";
+	$options = array(
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES => false
+	);
+	$conn = null;
 
-		</style>
-	</head>
-	<body>
-		<button id="button1" value="First time" onclick="initialTime()">First time!</button>
-		<div id="timerWindow"></div>
+	try {
+		$conn = new PDO($dsn, $username, $password, $options);
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+	}
 
-		<button id="button2" value="Press me" onclick="showTimeDiff()">Second time!</button>
-		<div id="secondTimerWindow"></div>
-
-		<button id="calculate" value="Calculate" onclick="calculateDiff()">Calculate diff</button>
-		<div id="diffWindow"></div>
-		<div id="arrayWindow"></div>
-
-		<!-- Javascript file -->
-
-			<?php
-		        $pdo = new PDO('mysql:host=wwwlab.iit.his.se;dbname=a17towan_examensarbete', 'sqllab', 'Tomten2009');
-		        $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-		        $pdo->exec("SET CHARACTER SET 'utf8'");
-
-		       	echo "<form class='form1' action='timer_and_database_PHP.php' method='post'>";
-				echo "ID:<br><input name='deltagareID' id='deltagareID' required='required' placeholder='ID' autocomplete='off'><br>";
-				echo "Start time:<br><input  name='startTid' id='startTid' required='required' placeholder='start tid'><br>";
-				echo "End time:<br><input  name='slutTid' id='slutTid' required='required' placeholder='slut tid'><br>";
-				echo "<button placeholder='submit' id='formBtn' class='insertBtn'>hey</button>";
-				echo "</form>";
-
-
-		        if(isset($_POST['deltagareID'])){
-		        	$querystring='INSERT INTO test1(deltagareID, startTid, slutTid) VALUES (:deltagareID, :startTid, :slutTid);';
-		        	$stmt = $pdo->prepare($querystring);
-		        	$stmt->bindParam(':deltagareID', $_POST['deltagareID']);
-		        	$stmt->bindParam(':startTid', $_POST['startTid']);
-		        	$stmt->bindParam(':slutTid', $_POST['slutTid']);
-		        	$stmt->execute();
-		        }
-
-		        echo "<table>";
-		        	echo "<tr>";
-		        		echo "<th>ID</th>";
-		        		echo "<th>Start time</th>";
-		        		echo "<th>End time</th>";
-		        	echo "</tr>";
-		        foreach($pdo->query("SELECT * FROM test1") as $row){
-		        	echo "<tr>";
-		        		echo "<td id='test'>".$row['deltagareID']."</td>";
-		        		echo "<td>".$row['startTid']."</td>";
-		        		echo "<td>".$row['slutTid']."</td>";
-		        	echo "</tr>";
-		        }
-		        echo "</tr>";
-		       echo "</table>";
-		    ?>
-		 <script src="timer_and_database_JS.js"></script>
-
-	   </body>
-	   </html>
+	if(isset($_POST["deltagareID"])) {
+		try {
+			$values = [
+				$_POST["deltagareID"],
+				$_POST["startTid"],
+				$_POST["slutTid"]
+			];
+			$sql = "INSERT INTO test1(deltagareID, startTid, slutTid) VALUES (?, ?, ?);";
+			$stmt = $conn->prepare($sql);
+			$stmt->execute($values);
+			echo "Successfully inserted.";
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
+?>
