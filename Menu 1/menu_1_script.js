fee304a192a2585dd7299a3cf1bb16e0228e0768
a17towan <a17towan@student.his.se>
2021-04-04 17:13:34 +0200

    Added separate files for menu 1 bottom
    Added separate files for menu 1 bottom. The "timer_and_database_bottom_JS.js" and "timer_and_database_bottom_PHP.php" that need to be separate because the tests will be different, and the results will be sent into different tables.
    
    Menu 1 bottom now also had the front page "overlay" with a startbutton, the form, and the ability to send into the database. It is currently not possible to send the results into the database because of the MySQL database structure.

--- Menu 1/menu_1_script.js
+++ Menu 1/menu_1_script.js
@@ -42,35 +42,6 @@ document.getElementById("slutTid").value = result;
 document.getElementById("submit").click();
 }
 
-// Ajax insert into database
-function insert(startTid, slutTid) {
-return $.ajax({
-  type: "POST",
-  url: "./timer_and_database_PHP.php",
-  data: { 
-    //deltagareID,
-    startTid,
-    slutTid
-  }
-});
-}
-
-// Stop the form from reloading the page on submit
-const form = document.querySelector("form");
-form.addEventListener("submit", e => {
-e.preventDefault();
-console.log("SUBMIT NO REFRESH");
-//const deltagareID = document.querySelector(`[name="deltagareID"]`).value;
-const startTid = document.querySelector(`[name="startTid"]`).value;
-const slutTid = document.querySelector(`[name="slutTid"]`).value;
-insert(
-  startTid, slutTid
-).done((response) => {
-  console.log(response);
-});
-});
-
-
 // Menu arrow and hierarchy toggle
 var toggler = document.getElementsByClassName("arrow");
 var i;
