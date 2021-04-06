a7797239fc1a2c25298e21c770f29ef1045f0690
a17towan <49142028+a17towan@users.noreply.github.com>
2021-04-06 19:25:10 +0200

    Removed auto_increment because it is not needed

--- Database/examensarbete_databas.sql
+++ Database/examensarbete_databas.sql
@@ -3,7 +3,7 @@ create database a17towan_examensarbete;
 use a17towan_examensarbete;
 
 create table test1( 
-deltagareID int NOT NULL AUTO_INCREMENT,
+deltagareID int NOT NULL,
 startTid varchar (100),
 slutTid varchar (100),
 primary key (deltagareID)
@@ -21,15 +21,17 @@ create table test3( -- Code tabell
 ID int,
 startTid varchar (100),
 slutTid varchar (100),
-primary key (ID)
+primary key (ID),
+foreign key (ID) references test1(deltagareID)
 )engine=innodb;
 
 create table test4( -- Code tabell 
 ID int,
 startTid varchar (100),
 slutTid varchar (100),
-primary key (ID)
+primary key (ID),
+foreign key (ID) references test1(deltagareID)
 )engine=innodb;
 
-INSERT INTO test1(startTid, slutTid) values ('2021-03-31 00:00:01', '2021-03-01 00:00:10');
 SELECT * FROM test1;
+SELECT * FROM test2;
